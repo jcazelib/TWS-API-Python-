c3d54bc9c3d2f3c68df3816e03e3e8dcf46740da
jcazelib <122401700+jcazelib@users.noreply.github.com>
2024-03-11 13:49:48 -0500

    Add files via upload

---
+++ F1 - requestFA.py
@@ -0,0 +1,39 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+import time
+
+port = 7497 
+
+
+class TestApp(EClient, EWrapper):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId: OrderId):
+        self.requestFA(1)                    #Request FA group info.
+        self.requestFA(2)                    #Request FA profile info.
+        #self.requestFA(3)
+
+
+    def receiveFA(self, faData: FaDataType, cxml: str):
+        if faData==1:
+            print("FA Group (1) info received:")
+            print(cxml)
+            print("")
+        elif faData==2:
+            print("FA Profile (2) info received:")
+            print(cxml)
+            print("")
+        elif faData==3:
+            print("FA Alias (3) info received:")
+            print(cxml)      
+            print("")
+        
+
+
+app = TestApp()
+app.connect("127.0.0.1", port, 1001)
+time.sleep(2)
+app.run()
+
