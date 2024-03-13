96dd678b780b40b7f92004fa3055e1ed82d2aaaa
jcazelib <122401700+jcazelib@users.noreply.github.com>
2024-03-13 10:14:02 -0500

    Add files via upload

---
+++ F4 - replaceFA (FA Groups).py
@@ -0,0 +1,77 @@
+"""
+Modify/Create an FA Group (i.e. a group of U-accounts) via replaceFA - https://www.ibkrguides.com/tws/usersguidebook/financialadvisors/create%20an%20account%20group%20for%20share%20allocation.htm?Highlight=group | https://ibkrcampus.com/ibkr-api-page/trader-workstation-api/#replace-fa
+    TWS -> Advisor Setup (top-left corner) -> Create Group / Edit / etc.
+    
+Available allocation methods:
+   Equal / AvailableEquity / NetLiq
+
+Replace the DU-account numbers below with your own U-accounts or DU-accounts:
+    "<String>DU2372888</String>"                      
+    "<String>DU2372889</String>" 
+"""
+
+from ibapi.client import *
+from ibapi.wrapper import *
+import time
+
+port = 7497 
+
+class TestApp(EClient, EWrapper):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId: OrderId):
+        self.nextValidOrderId = orderId
+        
+        FaThreeGroups = "".join(("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"               
+                    ,"<ListOfGroups>"            
+                                
+                        , "<Group>"                                              
+                            , "<name>EqualQuantity</name>"              #Name of group 1        
+                            , "<ListOfAccts varName=\"list\">"               
+                                , "<String>DU2372888</String>"          #U-account IDs for group 1              
+                                , "<String>DU2372889</String>"            
+                            , "</ListOfAccts>"                               
+                            , "<defaultMethod>Equal</defaultMethod>"    #Set the allocation method here - Equal / AvailableEquity / NetLiq
+                        , "</Group>"                                    
+                                   
+                        , "<Group>"                                              
+                            , "<name>NetLiquidation</name>"                     
+                            , "<ListOfAccts varName=\"list\">"               
+                                , "<String>DU2372888</String>"                                
+                                , "<String>DU2372889</String>"            
+                            , "</ListOfAccts>"                               
+                            , "<defaultMethod>NetLiq</defaultMethod>"     
+                        , "</Group>"                                    
+                    
+                        , "<Group>"                                              
+                            , "<name>AvailableEquity</name>"                      
+                            , "<ListOfAccts varName=\"list\">"               
+                                , "<String>DU2372888</String>"                                
+                                , "<String>DU2372889</String>"            
+                            , "</ListOfAccts>"                               
+                            , "<defaultMethod>AvailableEquity</defaultMethod>"     
+                        , "</Group>"      
+                                                                         
+                        
+                    , "</ListOfGroups>"))
+        
+        
+        self.replaceFA(orderId, 1, FaThreeGroups)                              #1 for Groups | 2 for Profiles    
+        print("Processing replaceFA() request", orderId, "...")
+        time.sleep(2)
+        print("")
+        print("")
+        print("New FA Groups:")
+        self.requestFA(FaDataTypeEnum.GROUPS)
+        time.sleep(2)
+        self.disconnect()
+
+    def receiveFA(self, faData: FaDataType, cxml: str):
+        print(cxml)
+
+app = TestApp()
+app.connect("127.0.0.1", port, 1)
+time.sleep(2)
+app.run()
---
+++ F5 - replaceFA (FA Profiles).py
@@ -0,0 +1,114 @@
+"""
+Modify/Create an FA Profile (i.e. a group of U-accounts) via replaceFA - https://www.ibkrguides.com/tws/usersguidebook/financialadvisors/create%20an%20account%20group%20for%20share%20allocation.htm?Highlight=group | https://ibkrcampus.com/ibkr-api-page/trader-workstation-api/#replace-fa
+    TWS -> Advisor Setup (top-left corner) -> Create Group / Edit / etc.
+
+Type:
+     1 is Percent Based | 2 is Ratio Based | 3 is Contracts/Shares | 4 is Monetary Amount              
+
+Replace the DU-account numbers below with your own U-accounts or DU-accounts:
+        "<acct>DU2372888</acct>"                                
+        "<amount>2.0</amount>"        
+   "</Allocation>"                        
+   "<Allocation>"                         
+       "<acct>DU2372889</acct>"                                
+       "<amount>1.0</amount>"
+"""
+
+from ibapi.client import *
+from ibapi.wrapper import *
+import time
+
+port = 7497 
+
+class TestApp(EClient, EWrapper):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId: OrderId):
+        self.nextValidOrderId = orderId
+        
+        FaFourProfile = "".join(("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"              
+                    , "<ListOfAllocationProfiles>"    
+                                                   
+                        , "<AllocationProfile>"                                  
+                            , "<name>PERCENT</name>"                   
+                            , "<type>1</type>"    
+                            , "<ListOfAllocations varName=\"listOfAllocations\">"
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372888</acct>"                                
+                                    , "<amount>80.0</amount>"        
+                                , "</Allocation>"                        
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372889</acct>"                                
+                                    , "<amount>20.0</amount>"        
+                                , "</Allocation>"                        
+                            , "</ListOfAllocations>"                         
+                        , "</AllocationProfile>"    
+                        
+                        , "<AllocationProfile>"                                  
+                            , "<name>RATIO</name>"                   
+                            , "<type>2</type>"    
+                            , "<ListOfAllocations varName=\"listOfAllocations\">"
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372888</acct>"                                
+                                    , "<amount>5.0</amount>"        
+                                , "</Allocation>"                        
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372889</acct>"                                
+                                    , "<amount>4.0</amount>"        
+                                , "</Allocation>"                        
+                            , "</ListOfAllocations>"                         
+                        , "</AllocationProfile>" 
+                        
+                        , "<AllocationProfile>"                                  
+                            , "<name>CONTRACTS/SHARES</name>"                   
+                            , "<type>3</type>"                    
+                            , "<ListOfAllocations varName=\"listOfAllocations\">"
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372888</acct>"                                
+                                    , "<amount>2.0</amount>"        
+                                , "</Allocation>"                        
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372889</acct>"                                
+                                    , "<amount>1.0</amount>"        
+                                , "</Allocation>"                        
+                            , "</ListOfAllocations>"                         
+                        , "</AllocationProfile>"                            
+                        
+                        , "<AllocationProfile>"                                  
+                            , "<name>CASH</name>"                   
+                            , "<type>4</type>"    
+                            , "<ListOfAllocations varName=\"listOfAllocations\">"
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372888</acct>"                                
+                                    , "<amount>5.0</amount>"        
+                                , "</Allocation>"                        
+                                , "<Allocation>"                         
+                                    , "<acct>DU2372889</acct>"                                
+                                    , "<amount>4.0</amount>"        
+                                , "</Allocation>"                        
+                            , "</ListOfAllocations>"                         
+                        , "</AllocationProfile>" 
+                                                
+                                        
+        , "</ListOfAllocationProfiles>"))
+        
+        
+        self.replaceFA(orderId, 2, FaFourProfile)                              #1 for Groups | 2 for Profiles    
+        print("Processing replaceFA() request", orderId, "...")
+        time.sleep(2)
+        print("")
+        print("")
+        print("New FA Profiles:")
+        self.requestFA(FaDataTypeEnum.PROFILES)
+        time.sleep(2)
+        self.disconnect()
+        
+    def receiveFA(self, faData: FaDataType, cxml: str):
+        print(cxml)
+
+app = TestApp()
+app.connect("127.0.0.1", port, 1)
+time.sleep(2)
+app.run()
