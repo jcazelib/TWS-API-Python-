7bf28af24da71f0e4b2487b57af547663a5fa5c7
jcazelib <122401700+jcazelib@users.noreply.github.com>
2024-03-19 09:20:42 -0500

    Add files via upload

--- A5 - Futures Chain.py
+++ A5 - Futures Chain.py
@@ -2,7 +2,7 @@
 Retreive the ES Futures chain (Contract IDs / localSymbols) for various ES Futures contracts which are not yet expired - https://ibkrcampus.com/ibkr-api-page/trader-workstation-api/#option-chain
 
 Uses reqContractDetails
-"""s
+"""
 
 from ibapi.client import *
 from ibapi.wrapper import *
